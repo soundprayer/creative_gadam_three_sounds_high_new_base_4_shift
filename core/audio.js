57900d5e1fd8befb60f7b71d4836acb2d415f034
maciejgrosz <maciej.groszyk8@gmail.com>
2025-03-29 23:56:38 +0100

    cleaning audio.js

--- core/audio.js
+++ core/audio.js
@@ -1,4 +1,3 @@
-// audio.js
 function setupOscillators() {
     osc1 = new p5.Oscillator('sine');
     osc2 = new p5.Oscillator('triangle');
@@ -12,93 +11,67 @@ function setupReverb() {
     reverb3 = new p5.Reverb();
     reverb4 = new p5.Reverb();
 
-    [reverb1, reverb2, reverb3, reverb4].forEach((rev, i) => {
-        rev.set(3, 2);
-        rev.drywet(reverbDryWet[i + 1]);
+    const oscillators = [osc1, osc2, osc3, osc4];
+    const reverbs = [reverb1, reverb2, reverb3, reverb4];
+
+    // Configure each reverb and connect it to its oscillator
+    reverbs.forEach((reverb, index) => {
+        const soundId = index + 1;
+
+        reverb.set(3, 2); // default time & decay
+        reverb.drywet(reverbDryWet[soundId]);
+
+        oscillators[index].disconnect();
+        reverb.process(oscillators[index]);
     });
-
-    osc1.disconnect();
-    osc2.disconnect();
-    osc3.disconnect();
-    osc4.disconnect();
-
-    reverb1.process(osc1);
-    reverb2.process(osc2);
-    reverb3.process(osc3);
-    reverb4.process(osc4);
 }
 
-function updateSound(sound, x, y) {
-    const margin = 5; // Leave a margin for easier clicking
-
-    x = constrain(x, margin, width - margin);
-    y = constrain(y, margin, height - margin);
+function updateSound(soundId, x, y) {
+    const margin = 5;
     const startMessage = document.getElementById('startMessage');
     if (startMessage) startMessage.style.display = 'none';
 
-    if (y === undefined || y === null) y = height;
     if (!height) return;
+    if (y === undefined || y === null) y = height;
 
-    let amp = map(y, height, 0, 0, 0.8);
-    let minFreq = 40;
-    let maxFreq = 1000;
-    let freq = minFreq * Math.pow(maxFreq / minFreq, x / width);
+    x = constrain(x, margin, width - margin);
+    y = constrain(y, margin, height - margin);
 
-    if (sound === 1) {
-        osc1.freq(freq, freqTransitionTimes[1]);
-        osc1.amp(amp, AMP_TRANSITION_TIME);
-        iconX1 = x;
-        iconY1 = y;
-        freq1 = freq;
-        amp1 = amp;
+    const amp = map(y, height, 0, 0, 0.8);
+    const minFreq = 40;
+    const maxFreq = 1000;
+    const freq = minFreq * Math.pow(maxFreq / minFreq, x / width);
 
-        // 👇 Start playing if it wasn’t yet
-        if (!isPlaying1) {
-            osc1.start();
-            isPlaying1 = true;
-        }
+    const oscMap = {
+        1: osc1,
+        2: osc2,
+        3: osc3,
+        4: osc4
+    };
 
-    } else if (sound === 2) {
-        osc2.freq(freq, freqTransitionTimes[2]);
-        osc2.amp(amp, AMP_TRANSITION_TIME);
-        iconX2 = x;
-        iconY2 = y;
-        freq2 = freq;
-        amp2 = amp;
+    const playingMap = {
+        1: () => { if (!isPlaying1) { osc1.start(); isPlaying1 = true; } },
+        2: () => { if (!isPlaying2) { osc2.start(); isPlaying2 = true; } },
+        3: () => { if (!isPlaying3) { osc3.start(); isPlaying3 = true; } },
+        4: () => { if (!isPlaying4) { osc4.start(); isPlaying4 = true; } }
+    };
 
-        if (!isPlaying2) {
-            osc2.start();
-            isPlaying2 = true;
-        }
+    oscMap[soundId].freq(freq, freqTransitionTimes[soundId]);
+    oscMap[soundId].amp(amp, AMP_TRANSITION_TIME);
 
-    } else if (sound === 3) {
-        osc3.freq(freq, freqTransitionTimes[3]);
-        osc3.amp(amp, AMP_TRANSITION_TIME);
-        iconX3 = x;
-        iconY3 = y;
-        freq3 = freq;
-        amp3 = amp;
-
-        if (!isPlaying3) {
-            osc3.start();
-            isPlaying3 = true;
-        }
-
-    } else if (sound === 4) {
-        osc4.freq(freq, freqTransitionTimes[4]);
-        osc4.amp(amp, AMP_TRANSITION_TIME);
-        iconX4 = x;
-        iconY4 = y;
-        freq4 = freq;
-        amp4 = amp;
-
-        if (!isPlaying4) {
-            osc4.start();
-            isPlaying4 = true;
-        }
+    // Explicitly update global variables
+    if (soundId === 1) {
+        iconX1 = x; iconY1 = y; freq1 = freq; amp1 = amp;
+    } else if (soundId === 2) {
+        iconX2 = x; iconY2 = y; freq2 = freq; amp2 = amp;
+    } else if (soundId === 3) {
+        iconX3 = x; iconY3 = y; freq3 = freq; amp3 = amp;
+    } else if (soundId === 4) {
+        iconX4 = x; iconY4 = y; freq4 = freq; amp4 = amp;
     }
-}
 
+    playingMap[soundId]();
+}
 
 // Keep all window.* reverb update functions
 window.addEventListener('DOMContentLoaded', (event) => {
